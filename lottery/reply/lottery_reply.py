415e1a14cf79ca5c77357b4dfe98db101f1e6894
pcyo <feng79624@gmail.com>
2021-01-10 10:26:36 +0000

    Add get ticket from database

--- lottery/reply/lottery_reply.py
+++ lottery/reply/lottery_reply.py
@@ -1,36 +1,20 @@
 from ..data.lottery import Lottery, LotteryScraper, LotteryTicket, LotteryChecker
 from ..messages.lottery_message import LotteryMessage
 from linebot.models import FlexSendMessage
+from feature.DBNumber import NumberTbl
 
 
 class LotteryReply:
-    def _mock_date_tickets(self, lottery):
-        return [
-            LotteryTicket(lottery.drawing_date, lottery.drawing_date, [
-                          1, 2, 3, 4, 5, 6], '自選'),
-            LotteryTicket(lottery.drawing_date, lottery.drawing_date, [
-                          5, 14, 40, 44, 45, 32], '電選')
-        ]
-
     def _get_tickets_by_date(self, date):
-        pass
-
-    def _mock_month_tickets(self, lottery_list):
-        first_list = [LotteryTicket(lottery.drawing_date, lottery.drawing_date, [
-            1, 2, 3, 4, 5, 6], '自選') for lottery in lottery_list]
-        second_list = [LotteryTicket(lottery.drawing_date, lottery.drawing_date, [
-            5, 14, 40, 44, 45, 32], '電選') for lottery in lottery_list]
-        first_list.pop()
-        first_list.pop(1)
-        first_list.pop(2)
-        second_list.pop()
-        second_list.pop(1)
-        return first_list + second_list
+        tickets = [LotteryTicket(draw_date, draw_date, [int(number.strip()) for number in numbers.split(',')],
+                                 '自選' if lottery_type == 0 else '電選')
+                   for draw_date, numbers, lottery_type in NumberTbl.findNm(None, date, None)]
+        return tickets
 
     def _get_tickets_by_month(self, year, month):
         pass
 
-    def _generate_ticket_prize_pair_list(self, lottery: Lottery, tickets: LotteryTicket):
+    def _generate_ticket_prize_pair_list(self, lottery, tickets):
         return [(ticket, LotteryChecker().check(lottery, ticket))
                 for ticket in tickets if lottery.drawing_date == ticket.drawing_date]
 
@@ -38,7 +22,6 @@ class LotteryReply:
         if type == 'get_latest':
             lottery = LotteryScraper().scrape_latest()
             tickets = self._get_tickets_by_date(lottery.drawing_date) or []
-            #tickets = self._mock_date_tickets(lottery)
 
             pairs = self._generate_ticket_prize_pair_list(lottery, tickets)
             contents = LotteryMessage(lottery, pairs).get_message()
@@ -49,7 +32,6 @@ class LotteryReply:
     def get_reply_message_for_month(self, year, month):
         lottery_list = LotteryScraper().scrape_by_month(year, month)
         tickets = self._get_tickets_by_month(year, month) or []
-        #tickets = self._mock_month_tickets(lottery_list) or []
 
         contents = []
         for lottery in lottery_list:
