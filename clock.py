2ee937defdbc7e76031db4c694eb7961982e422d
Sio.li <sio.li@softbi.com>
2021-07-06 13:54:31 +0800

    refactor: linebotConfig and scheduler be module

--- clock.py
+++ clock.py
@@ -1,17 +1,66 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
 import urllib.request
 import urllib.error
 import os
+from config.lineBotConfig import LinbotConfig
 
-scheduler = BlockingScheduler()
+from menu import notifyMenu
+
+scheduler = BackgroundScheduler()
 url = os.getenv('APP_URL')
-class Scheduler:
-    def wake_me_up_job():
+print('start clock')
+
+@scheduler.scheduled_job('interval',  minutes=1)
+def wake_me_up_job():
+    try:
+        urllib.request.urlopen(url)
+        print('Wake me up when september ends')
+    except urllib.error.URLError as e:
+        print('Request to url:', url, 'error.')
+        print('Reason:', e.reason)
+    except:
+        print('Invalid url:', url)
+
+
+
+@scheduler.scheduled_job('cron', day_of_week='tue,fri', hour='22', minute='00', timezone='Asia/Taipei')
+def lottery649_drawing_job():
+    print("開始通知")
+    subscriber_ids = notifyMenu.getIdAll()
+    if (len(subscriber_ids) == 0):
+        print("沒有通知")
+        return "OK"
+
+    for id in subscriber_ids:
         try:
-            urllib.request.urlopen(url)
-            print('Wake me up when september ends')
-        except urllib.error.URLError as e:
-            print('Request to url:', url, 'error.')
-            print('Reason:', e.reason)
-        except:
-            print('Invalid url:', url)
+            line_bot_api = LinbotConfig.get_line_bot_api()
+            line_bot_api.push_message(id[0],  FlexSendMessage(
+                alt_text = "最新中獎號碼",
+                contents = rewMenu.newestReward()
+            ))
+        except Exception:
+            continue        
+
+@scheduler.scheduled_job('cron', day='20', hour='10', minute='30', timezone='Asia/Taipei')
+def pickLotteryNumber_job():
+    print("誰去買樂透")
+    subscriber_ids = notifyMenu.getIdAll()
+    if (len(subscriber_ids) == 0):
+        print("沒有通知")
+        return "OK"
+
+    for id in subscriber_ids:
+        try:
+            line_bot_api = LinbotConfig.get_line_bot_api()
+            line_bot_api.push_message(id[0],  FlexSendMessage(
+                    alt_text = "誰去買樂透",
+                    contents = shiftMenu.shift()
+                ))
+        except Exception as e:
+            print(str(e))
+            continue         
+
+scheduler.start()
---
+++ config/lineBotConfig.py
@@ -0,0 +1,42 @@
+
+import os
+import sys
+import re
+from linebot import (
+    LineBotApi, WebhookParser
+)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+class LinbotConfig:
+
+    line_bot_api = None
+    parser = None
+    
+    # get channel_secret and channel_access_token from your environment variable
+    @classmethod
+    def initialize(cls, **kwargs):
+        cls.line_bot_api = LineBotApi(channel_access_token)
+        cls.parser = WebhookParser(channel_secret)
+
+    @classmethod
+    def get_line_bot_api(cls):
+        return cls.line_bot_api
+    
+    @classmethod
+    def get_parser(cls):
+        return cls.parser
+
+  
+    
+    
+
+
