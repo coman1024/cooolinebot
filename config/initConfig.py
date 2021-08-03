c60e2b643563c2780bf8e7a8fedf0dd8b46e1e1b
Sio.li <sio.li@softbi.com>
2021-08-03 10:32:50 +0800

    fix: scheduler using background Schedule and initiallize schedule

--- config/initConfig.py
+++ config/initConfig.py
@@ -1,7 +1,11 @@
 from config.dbConfig import Database
 from config.lineBotConfig import LinbotConfig
+from clock import SchdulerJob
 
 
 def initialize():
   Database.initialize()
   LinbotConfig.initialize()
+  SchdulerJob.initialize()
+
+  
